4070db1aae81d2379e8dbab2ed9044426221098f
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-01-13 17:42:28 -0300

    Mapeo de MovieDB

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/constant/environment.dart';
 import 'package:flutter/material.dart';
 
 
@@ -9,10 +8,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Environment.theMovieDbKey),
-      ),
+    return const Scaffold(
+      body: Placeholder(),
     );
   }
 }
