000b214451e99dacec217914912ee915b150e667
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-01-16 17:45:40 -0300

    Evitar peticiones simultaneas

--- lib/presentation/providers/movies/movies_providers.dart
+++ lib/presentation/providers/movies/movies_providers.dart
@@ -19,7 +19,7 @@ typedef MovieCallback = Future<List<Movie>> Function({ int page});
 class MoviesNotifier extends StateNotifier<List<Movie>> {
 
   int currentPage = 0;
-
+  bool isLoading = false;
   MovieCallback fetchMoreMovies;
 
 
@@ -28,12 +28,17 @@ class MoviesNotifier extends StateNotifier<List<Movie>> {
   }): super([]);
 
   Future<void> loadNextPage() async {
+    if ( isLoading ) return;
 
+    isLoading = true;
+    print('Loading more movies');
     currentPage++;
 
     final List<Movie> movies = await fetchMoreMovies(page: currentPage);
 
     state = [...state, ...movies];
+    await Future.delayed(const Duration(milliseconds: 300));
+    isLoading = false;
   }
 
   
--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -37,7 +37,7 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
 
-    // final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
     final slideShowMovies = ref.watch(moviesSlideshowProvider);
     // if( nowPlayingMovies.length == 0 ) return CircularProgressIndicator();
     return Column(
@@ -47,6 +47,16 @@ class _HomeViewState extends ConsumerState<_HomeView> {
 
 
         MoviesSlideshow(movies: slideShowMovies),
+
+        MovieHorizontalListview(
+          movies: nowPlayingMovies,
+          title: 'En cines',
+          subTitle: 'Lunes 20',
+          loadNextPage: () {
+
+            ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+          },
+          ),
 
 
         // Expanded(
