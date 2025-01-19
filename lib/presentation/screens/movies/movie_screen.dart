dfd50a685e5d10876b844e3130c0ceca2baaa2e0
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-01-19 00:00:00 -0300

    DeepLink y movieid recibido con params goroute

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -13,6 +13,11 @@ class MovieScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MovieID: $movieId'),
+      ),
+    );
+
   }
 }
