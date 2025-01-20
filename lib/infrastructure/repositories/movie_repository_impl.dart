2bab776884182552a4d6be8c61461aaf987d872a
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-01-19 23:01:39 -0300

    Implementacion de metodo en repositorio getMovieById

--- lib/infrastructure/repositories/movie_repository_impl.dart
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -35,6 +35,13 @@ class MovieRepositoryImpl extends MoviesRepository {
     return datasource.getUpcoming(page: page);
 
   }
+  
+  @override
+  Future<Movie> getMovieById(String id) {
+
+    return datasource.getMovieById(id);
+
+  }
 
 
 
