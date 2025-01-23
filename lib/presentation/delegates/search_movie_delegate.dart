bfb6799977403f4cbe622ce3639953dad7305ce2
Kevin Cristensen <ke.cristensen@duocuc.cl>
2025-01-23 16:21:23 -0300

    SearchDelegate -> BuildActions animate & BuildLeading Icon

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -2,23 +2,38 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 
-class SearchMovieDelegate extends SearchDelegate{
+
+
+class SearchMovieDelegate extends SearchDelegate<Movie?>{
 
   @override
   String get searchFieldLabel => 'Buscar Pelicula';
-  
+
   @override
   List<Widget>? buildActions(BuildContext context) {
 
     return [
-      const Text('BuildActions'),
+
+      // if (query.isNotEmpty)
+        FadeIn(
+          animate: query.isNotEmpty,
+          child: IconButton(
+            onPressed: () => query = '',
+            icon: const Icon(Icons.clear)
+          ),
+        )
     ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('BuildLeading');
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: const Icon(Icons.arrow_back_ios_new_rounded)
+      );
   }
 
   @override
